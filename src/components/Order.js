33e981b28cdfbea3ae61a24e0cca8f891b7a2ea1
sayands <sayandsarkar.1997@gmail.com>
2018-03-30 19:42:18 +0530

    Added Initial Animations For React Components

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { formatPrice } from "../helpers";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Order extends React.Component {
 	renderOrder = key => {
@@ -9,19 +10,42 @@ class Order extends React.Component {
 		if (!fish) return null; //make sure there is fish
 		if (!isAvailable) {
 			return (
-				<li key={key}>
-					Sorry {fish ? fish.name : "fish"} is no longer available
-				</li>
+				<CSSTransition
+					classNames="order"
+					key={key}
+					timeout={{ enter: 250, exit: 250 }}
+				>
+					<li key={key}>
+						Sorry {fish ? fish.name : "fish"} is no longer available
+					</li>
+				</CSSTransition>
 			);
 		}
 		return (
-			<li key={key}>
-				{count} lbs {fish.name}
-				{formatPrice(count * fish.price)}
-				<button onClick={() => this.props.removeFromOrder(key)}>
-					&times;
-				</button>
-			</li>
+			<CSSTransition
+				classNames="order"
+				key={key}
+				timeout={{ enter: 500, exit: 500 }}
+			>
+				<li key={key}>
+					<span>
+						<TransitionGroup component="span" className="count">
+							<CSSTransition
+								classNames="count"
+								key={count}
+								timeout={{ enter: 5000, enter: 5000 }}
+							>
+								<span>{count}</span>
+							</CSSTransition>
+						</TransitionGroup>
+						lbs {fish.name}
+						{formatPrice(count * fish.price)}
+						<button onClick={() => this.props.removeFromOrder(key)}>
+							&times;
+						</button>
+					</span>
+				</li>
+			</CSSTransition>
 		);
 	};
 
@@ -41,7 +65,9 @@ class Order extends React.Component {
 		return (
 			<div className="order-wrap">
 				<h2>Order</h2>
-				<ul className="order">{orderIds.map(this.renderOrder)}</ul>
+				<TransitionGroup component="ul" className="order">
+					{orderIds.map(this.renderOrder)}
+				</TransitionGroup>
 				<div className="total">
 					Total:
 					<strong>{formatPrice(total)}</strong>
